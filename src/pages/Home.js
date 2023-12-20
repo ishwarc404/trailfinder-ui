f938a8efa99ee19e29a8750805254dc3ccb59bff
Ishwar Choudhary <ishwarc404@gmail.com>
2023-12-20 16:58:35 -0700

    chore; connect to backend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,26 @@
-import Header from './header/header'
-import Searcharea from './searcharea/searcharea'
-
-
-//STYLES
-import './home.css';
+import React, { useState } from 'react';
+import Header from './header/header';
+import Searcharea from './searcharea/searcharea';
 import MapComponent from './mapcomponent/mapcomponent';
-
-
+import './home.css';
 
 function Home() {
+  const [selectedTrailCoords, setSelectedTrailCoords] = useState(null);
 
-
-    return (
-        <div className='home'>
-            <Header />
-            <br />
-            <div className='d-flex justify-content-center'>
-                <div>
-                    <Searcharea />
-                </div>
-                <div>
-                    <MapComponent />
-                </div>
-
-            </div>
-
+  return (
+    <div className='home'>
+      <Header />
+      <br />
+      <div className='d-flex justify-content-center'>
+        <div>
+          <Searcharea onTrailSelect={setSelectedTrailCoords} />
         </div>
-    );
+        <div>
+          <MapComponent trailCoords={selectedTrailCoords} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/pages/searcharea/searcharea.js
+++ src/pages/searcharea/searcharea.js
@@ -1,15 +1,57 @@
 import './searcharea.css';
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 import { Slider } from "@nextui-org/react";
 import {Button} from "@nextui-org/react";
-
 import {Tabs, Tab} from "@nextui-org/react";
+import {Listbox, ListboxItem} from "@nextui-org/react";
+import {ListboxWrapper} from "./ListboxWrapper";
 
-function Searcharea() {
 
-  const [distanceValue, setDistanceValue] = React.useState([2, 13]);
-  const [elevationGainValue, setGainValue] = React.useState([50, 150]);
+function Searcharea({ onTrailSelect }) {
+
+  const [distanceValue, setDistanceValue] = useState([2, 13]);
+  const [elevationGainValue, setGainValue] = useState([50, 150]);
+  const [trails, setTrails] = useState([]); // State to store the trails
+  const [selectedTrail, setSelectedTrail] = useState([]);
+
+  function fetchTrails() {
+    // Define the JSON object to send
+    const params = {
+      distance: distanceValue,
+      elevationGain: elevationGainValue
+    };
+
+    // Use a POST request if you want to send a JSON object in the request body
+    axios.post("http://127.0.0.1:5000/get-trails")
+      .then(response => {
+        // Handle the response data (list of trails)
+        setTrails(response.data);
+      })
+      .catch(error => {
+        // Handle any errors here
+        console.error('There was an error fetching the trails', error);
+      });
+  }
+
+  function handleTrailSelect(key) {
+    console.log(key)
+    for(let i = 0; i < trails.length ; i ++){
+      if(trails[i]["id"] == key){
+      onTrailSelect(trails[i]["coordinates"]); // Use the coordinates property
+    }
+    }
+    // const selectedItem = trails.find(trail => trail["id"] == key); // Assuming each trail has a unique 'id' property
+    // console.log(selectedItem); // Check the structure of the selected trail item
+  
+    // if (selectedItem && selectedItem.coordinates) {
+    // } else {
+    //   console.error('Selected trail item does not have coordinates property');
+    // }
+  }
+  
 
 
   return (
@@ -54,10 +96,33 @@ function Searcharea() {
         </div>
 
         <div  className='search-button'>
-          <Button color="default" varient="flat">
+          <Button 
+          color="default" 
+          varient="flat"
+          onClick={fetchTrails}
+          >
           Search
            </Button> 
         </div>
+      </div>
+
+      <div className="listbox-wrapper">
+        <Listbox
+          items={trails}
+          aria-label="Trails"
+          onAction={handleTrailSelect}
+        >
+          {(item) => (
+            <ListboxItem
+              key={item.id}
+              color={"default"}
+              className={""}
+            >
+              Distance: {(item.distance / 1000).toFixed(1) } km
+              Elevation: {Math.round(item.elevation) }
+            </ListboxItem>
+          )}
+        </Listbox>
       </div>
 
     </div>
